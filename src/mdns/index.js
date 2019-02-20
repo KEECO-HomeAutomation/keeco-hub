4045d9677f8966f28568a9cde0950bd6d2ba203c
uno20001 <>
2019-02-20 15:16:10 +0100

    rewritten mdns class using 'dnssd' package; advertising http and mqtt

--- src/mdns/index.js
+++ src/mdns/index.js
@@ -1,34 +1,50 @@
-import MulticastDNS from 'multicast-dns';
+import dnssd from 'dnssd';
 import InternalIP from 'internal-ip';
 import { log } from '../utils';
+import uuidv1 from 'uuid/v1';
 
 class Mdns {
 	constructor() {
-		this.response = [
-			{
-				name: 'hub.keeco.local',
-				type: 'A',
-				ttl: 300,
-				data: InternalIP.v4.sync()
-			}
-		];
+		this.id = uuidv1();
 	}
 
 	init(callback) {
-		this.mdns = new MulticastDNS();
-
-		this.mdns.on('query', query => {
-			log(
-				'MDNS',
-				'Received query with question: ' +
-					query.questions[0].name +
-					' Type: ' +
-					query.questions[0].type
-			);
-			this.mdns.respond(this.response);
+		this.httpAd = new dnssd.Advertisement(dnssd.tcp('http'), 8080, {
+			name: `keeco-hub-web-${this.id}`,
+			subtypes: ['_keeco-hub'],
+			txt: {
+				path: '/index.html'
+			}
 		});
 
-		this.mdns.respond(this.response, callback);
+		this.mqttAd = new dnssd.Advertisement(dnssd.tcp('mqtt'), 1883, {
+			name: `keeco-hub-mqtt-${this.id}`,
+			subtypes: ['_keeco-hub'],
+			txt: {
+				topic: '/register'
+			}
+		});
+
+		this.httpAd.on('error', err => {
+			log(
+				'MDNS',
+				`HTTP instance publishing ran into an error: ${err}`,
+				'error'
+			);
+		});
+
+		this.mqttAd.on('error', err => {
+			log(
+				'MDNS',
+				`MQTT instance publishing ran into an error: ${err}`,
+				'error'
+			);
+		});
+
+		this.httpAd.start();
+		this.mqttAd.start();
+
+		callback();
 	}
 }
 
