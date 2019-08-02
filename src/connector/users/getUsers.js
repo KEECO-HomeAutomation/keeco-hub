f782bada1c0c9afbc28c52a2e0f75ec11711f020
Fándly Gergő <gergo@systemtest.tk>
2019-08-02 20:57:12 +0300

    Promisify src/connector/users

--- src/connector/users/getUsers.js
+++ src/connector/users/getUsers.js
@@ -1,12 +1,8 @@
 const getUsers = conn => {
 	return new Promise((resolve, reject) => {
-		conn.db.all('SELECT id, username FROM users', {}, (err, rows) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(rows);
-			}
-		});
+		conn.db.all('SELECT id, username FROM users', {}).then(rows => {
+			resolve(rows);
+		}, reject);
 	});
 };
 
